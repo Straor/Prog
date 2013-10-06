6fea3f4d92f2d7ba4e77e4ef3c314177ea8fe5a6
Straor <victor@gilou.net>
2013-10-06 21:09:43 +0200

    travail sur programme 10

--- Python/prog10.py
+++ Python/prog10.py
@@ -2,13 +2,23 @@
 #programme qui fait deviner un nombre entre 1 et 100
 #amélioration à faire : vérifier la saisie de chaine
 import random
+import sys
+
 print("Salut ! ")
 print("j'ai choisi un nombre trouves le !")
 nombrechoisi=random.randint(1,100)
 gagne=False
 coups=0
 while(gagne==False):
-	essai=int(input("vas y tapes ton nombre ! \n"))
+	chaine=input("vas y tapes ton nombre ! \n")
+	if(chaine=="quit"):
+		print("au revoir")
+		sys.exit()	
+	try:
+		essai=int(chaine)
+	except:
+		print("caqca")
+		
 	if(essai>nombrechoisi):
 		print("trop grand")
 		coups=coups+1
@@ -25,4 +35,3 @@ if(coups<5):
 if (coups>=5 and coups<=10):
 	reponse="tu peux mieux faire"
 print(reponse)
-
