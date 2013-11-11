81b3a10aa7dba803090d14fadec72b6c74f2bfc4
victor <victor@oubounetou.(none)>
2013-11-11 17:59:56 +0100

    travail sur le programme des triangles

--- Python/prog19.py
+++ Python/prog19.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # programme qui demande les trois longueurs d'un triangle et qui donne ses spécificités
-# avoir une phrase quand on tape un caractere à la plasse d'un chiffre
-#traiter le cas où un triangle est impossile
+# avoir une phrase quand on tape un caractere à la place d'un chiffre
 chaine=input("quelle est la longueur du premier côté ? ")
 a=int(chaine)
 chaine=input("quelle est la longueur du deuxième côté ? ")
@@ -11,12 +10,9 @@ c=int(chaine)
 equilateral=False
 isocele=False
 rectangle=False
-quelconque=True
-if(a==b and b==c):
-	print("abc est un triangle équilatéral")
-	equilateral=True
-if(a==b or a==c or b==c and not equilateral):
-	print("abc est un triangle isocèle")
+quelconque=False
+impossible=False
+#plus grand coté...
 hypotenuse=a
 autrecote1=b
 autrecote2=c
@@ -28,11 +24,23 @@ if(c>hypotenuse):
 	hypotenuse=c
 	autrecote1=a
 	autrecote2=b
-if(hypotenuse**2==autrecote1**2+autrecote2**2):
+#impossible
+if(hypotenuse>=autrecote1+autrecote2):
+	impossible=True
+#rectangle	
+if(not impossible and hypotenuse**2==autrecote1**2+autrecote2**2):
 	rectangle=True
-	print("abc est un triangle rectangle")
-if(equilateral or isocele or rectangle):
-	quelconque=False
-if(quelconque):
-	print("abc est un triangle quelconque")
-
+#équilateral
+if(not impossible and a==b and b==c):
+	equilateral=True
+#isocele
+if(not impossible and (a==b or a==c or b==c) and not equilateral):
+	isocele=True
+#quelconque
+if(not impossible and not equilateral and not isocele and not rectangle):
+	quelconque=True
+print("equilateral : ",equilateral)
+print("isocele : ",isocele)
+print("rectangle : ",rectangle)
+print("quelconque : ",quelconque)
+print("impossible : ",impossible)
