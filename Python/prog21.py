8a0d2550891633606c7f7dc52463b950ed1fc712
victor <victor@oubounetou.(none)>
2013-11-24 15:46:09 +0100

    fin du prog sur les triangles et prog sur les nombre pairs

--- Python/prog21.py
+++ Python/prog21.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 #trouver les longueures d'un triangle rectangle et isocele
-chaine=input("longueure entiere du plus grand coté")
+chaine=input("longueure entiere du plus grand coté ")
 longueurduplusgrandcote=int(chaine)
-if longueurduplusgrandcote/2+1
+autrescotes=longueurduplusgrandcote/2+1
+while(longueurduplusgrandcote**2!=(autrescotes**2)*2):
+	autrescotes=autrescotes+1
+	if(autrescotes>=longueurduplusgrandcote):
+		longueurduplusgrandcote=longueurduplusgrandcote+1
+		autrescotes=longueurduplusgrandcote/2+1
+print("le triangle ayant pour coté",longueurduplusgrandcote , autrescotes , autrescotes , "est un triangle rectangle")
+
