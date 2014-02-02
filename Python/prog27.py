10ca1f1c26944427c687121349e48d33efcabf1e
Straor <victor@gilou.net>
2014-02-02 19:10:01 +0100

    amelioration du programme du pendu

--- Python/prog27.py
+++ Python/prog27.py
@@ -7,17 +7,24 @@ taille=len(mot)
 partiefinie=False
 chance=10
 motadeviner="_______"
+nombredelettresmanquantes=taille
+
 while(not partiefinie):
+	print(motadeviner)
 	print("tape une lettre : ")
 	proposition=input()
 	while(compteur!=taille):
 		if(proposition==mot[compteur]):
-			motadeviner[compteur]=proposition
-			print(motadeviner)
-		compteur=compteur+1	
+			motadeviner=motadeviner[0:compteur]+proposition+motadeviner[compteur+1:]
+			nombredelettresmanquantes=nombredelettresmanquantes-1
+		compteur=compteur+1
+	compteur=0
 	chance=chance-1
+	if(nombredelettresmanquantes==0):
+		partiefinie=True
+		print('tu as gagné')
 	print(chance)
 	if(chance==0):
 		partiefinie=True
 		print("tu as perdu")
-
+	
