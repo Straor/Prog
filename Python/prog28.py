e50ec3c3ea4009e1074e21b9f7b8ef2db929bb7b
Straor <victor@gilou.net>
2014-03-30 19:27:54 +0200

    simplification du code du programme 28 et continuation du travail

--- Python/prog28.py
+++ Python/prog28.py
@@ -16,61 +16,54 @@ B3=' '
 C1=' '
 C2=' '
 C3=' '
-def affichage(A1, A2, A3, B1, B2, B3, C1, C2, C3):
-	print("|", A1 ,"|", A2 ,"|", A3, "|")
-	print("-------------")
-	print("|", B1 ,"|", B2 ,"|", B3 ,"|")
-	print("-------------")
-	print("|", C1 ,"|" ,C2 ,"|", C3 ,"|")
 
-nombredetourajouer=5
 
-affichage(A1, A2, A3, B1, B2, B3, C1, C2, C3)
-while(nombredetourajouer>0):
-	if(A1==A2==A3 or B1==B2==B3 or C1==C2==C3 or A1==B2==C3 or A3==B2==C1
-	or A1==B1==C1 or A2==B2==C2 or A3==B3==C3):
-		print('bravo !!!')
+dico = {}
+dico['A1'] =' '
+dico['A2'] =' '
+dico['A3'] =' '
+dico['B1'] =' '
+dico['B2'] =' '
+dico['B3'] =' '
+dico['C1'] =' '
+dico['C2'] =' '
+dico['C3'] =' '
+
+def affichage(dico):
+	print("|", dico['A1'] ,"|", dico['A2'] ,"|", dico['A3'] ,"|")
+	print("-------------")
+	print("|", dico['B1'] ,"|", dico['B2'] ,"|", dico['B3'] ,"|")
+	print("-------------")
+	print("|", dico['C1'] ,"|", dico['C2'] ,"|", dico['C3'] ,"|")
+
+
+partiepasfinie=5
+joueur1=True
+
+partiepasfinie=True
+affichage(dico)
+while(partiepasfinie>0):
+	if(joueur1==True):
+		joueur="Joueur 1 "
+		signe='x'
 		
-	case=input('joueur 1 tu veux mettre ta croix dans quelle case ?')
-	if(case=='A1'):
-		A1='x'
-	if(case=='A2'):
-		A2='x'
-	if(case=='A3'):
-		A3='x'
-	if(case=='B1'):
-		B1='x'
-	if(case=='B2'):
-		B2='x'
-	if(case=='B3'):
-		B3='x'
-	if(case=='C1'):
-		C1='x'
-	if(case=='C2'):
-		C2='x'
-	if(case=='C3'):
-		C3='x'
-	affichage(A1, A2, A3, B1, B2, B3, C1, C2, C3)
+	else :
+		joueur='Joueur 2 '
+		signe='o'
+		
+	case=input(joueur + 'tu veux mettre ta ' + signe +' dans quelle case ?')	
 
-	case=input('joueur 2 tu veux mettre ton rond dans quelle case ?')
-	if(case=='A1'):
-		A1='o'
-	if(case=='A2'):
-		A2='o'
-	if(case=='A3'):
-		A3='o'
-	if(case=='B1'):
-		B1='o'
-	if(case=='B2'):
-		B2='o'
-	if(case=='B3'):
-		B3='o'
-	if(case=='C1'):
-		C1='o'
-	if(case=='C2'):
-		C2='o'
-	if(case=='C3'):
-		C3='o'
-	affichage(A1, A2, A3, B1, B2, B3, C1, C2, C3)
-	nombredetourajouer=nombredetourajouer-1
+	if(dico[case]!=' '):
+		print('tu vois pas que la case est deja occupée ????')
+	else: 		
+		dico[case]=signe
+	affichage(dico)
+	joueur1= not joueur1
+if(joueur==False):
+		affichage(dico)
+		
+
 print('égalité !!!!')
+
+
+
